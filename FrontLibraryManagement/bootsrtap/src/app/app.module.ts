c6a97cc168b8b48dc5a8983efac435fd55c32971
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-04-18 12:53:22 +0500

    tet

--- FrontLibraryManagement/bootsrtap/src/app/app.module.ts
+++ FrontLibraryManagement/bootsrtap/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
+import { DeleteuserComponent } from './deleteuser/deleteuser.component';
+import { GetallComponent } from './getall/getall.component';
+import { GetbyidComponent } from './getbyid/getbyid.component';
+import { UpdateuserComponent } from './updateuser/updateuser.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    DeleteuserComponent,
+    GetallComponent,
+    GetbyidComponent,
+    UpdateuserComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
